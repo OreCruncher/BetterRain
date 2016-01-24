f92445c4d4544e24c06b2d82aa88fed96bbfe51a
OreCruncher <tedjkaiser@gmail.com>
2016-01-24 07:01:16 -0800

    Cleanup rain command

--- src/main/java/org/blockartistry/mod/DynSurround/commands/CommandRain.java
+++ src/main/java/org/blockartistry/mod/DynSurround/commands/CommandRain.java
@@ -62,7 +62,7 @@ public final class CommandRain extends CommandBase {
 
 	@Override
 	public String getCommandUsage(final ICommandSender p_71518_1_) {
-		return "/rain <status | reset | 1-100 | <<setmax|setmin> 0-100>";
+		return "/rain <status | reset | reload | 1-100 | <<setmax|setmin> 0-100>";
 	}
 
 	@Override
