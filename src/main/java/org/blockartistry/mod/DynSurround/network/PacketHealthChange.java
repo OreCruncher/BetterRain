9471a6d5e17760399d5af1e611d2bb7acd01d261
OreCruncher <tedjkaiser@gmail.com>
2016-02-03 12:00:40 -0800

    Suppress player's popoffs

--- src/main/java/org/blockartistry/mod/DynSurround/network/PacketHealthChange.java
+++ src/main/java/org/blockartistry/mod/DynSurround/network/PacketHealthChange.java
@@ -24,6 +24,8 @@
 
 package org.blockartistry.mod.DynSurround.network;
 
+import java.util.UUID;
+
 import org.blockartistry.mod.DynSurround.client.DamageEffectHandler;
 import org.blockartistry.mod.DynSurround.client.DamageEffectHandler.HealthData;
 
@@ -34,6 +36,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class PacketHealthChange implements IMessage, IMessageHandler<PacketHealthChange, IMessage> {
 
+	private UUID entityId;
 	private float posX;
 	private float posY;
 	private float posZ;
@@ -53,12 +56,13 @@ public class PacketHealthChange implements IMessage, IMessageHandler<PacketHealt
 	}
 
 	public IMessage onMessage(final PacketHealthChange message, final MessageContext ctx) {
-		DamageEffectHandler.handleEvent(new HealthData(message.posX, message.posY, message.posZ, message.isCritical, message.amount));
+		DamageEffectHandler.handleEvent(new HealthData(message.entityId, message.posX, message.posY, message.posZ, message.isCritical, message.amount));
 		return null;
 	}
 
 	@Override
 	public void fromBytes(final ByteBuf buf) {
+		this.entityId = new UUID(buf.readLong(), buf.readLong());
 		this.posX = buf.readFloat();
 		this.posY = buf.readFloat();
 		this.posZ = buf.readFloat();
@@ -68,6 +72,8 @@ public class PacketHealthChange implements IMessage, IMessageHandler<PacketHealt
 
 	@Override
 	public void toBytes(final ByteBuf buf) {
+		buf.writeLong(this.entityId.getMostSignificantBits());
+		buf.writeLong(this.entityId.getLeastSignificantBits());
 		buf.writeFloat(this.posX);
 		buf.writeFloat(this.posY);
 		buf.writeFloat(this.posZ);
