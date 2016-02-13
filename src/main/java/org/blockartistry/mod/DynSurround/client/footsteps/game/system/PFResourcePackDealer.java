2b6868bbb466b110c549a40ee73d63eb1ceae7ed
OreCruncher <tedjkaiser@gmail.com>
2016-02-13 06:23:20 -0800

    Resource pack implementation

--- src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/game/system/PFResourcePackDealer.java
+++ src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/game/system/PFResourcePackDealer.java
@@ -24,55 +24,89 @@
 
 package org.blockartistry.mod.DynSurround.client.footsteps.game.system;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.blockartistry.mod.DynSurround.ModLog;
 import org.blockartistry.mod.DynSurround.Module;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.IResourcePack;
 import net.minecraft.client.resources.ResourcePackRepository;
+import net.minecraft.client.resources.data.IMetadataSection;
+import net.minecraft.client.resources.data.IMetadataSerializer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
 
 @SideOnly(Side.CLIENT)
 public class PFResourcePackDealer {
-	
-	// Used for existing packs that have been configured for Presence Footsteps
-	private final ResourceLocation pf_pack = new ResourceLocation("presencefootsteps", "pf_pack.json");
-	private final ResourceLocation acoustics = new ResourceLocation("presencefootsteps", "acoustics.json");
-	private final ResourceLocation blockmap = new ResourceLocation("presencefootsteps", "blockmap.cfg");
-	private final ResourceLocation primitivemap = new ResourceLocation("presencefootsteps", "primitivemap.cfg");
-	private final ResourceLocation variator = new ResourceLocation("presencefootsteps", "variator.cfg");
 
-	public List<ResourcePackRepository.Entry> findResourcePacks() {
+	// Used for existing packs that have been configured for Presence Footsteps
+	private final ResourceLocation pf_pack = new ResourceLocation("footsteps", "pf_pack.json");
+	private final ResourceLocation acoustics = new ResourceLocation("footsteps", "acoustics.json");
+	private final ResourceLocation blockmap = new ResourceLocation("footsteps", "blockmap.cfg");
+	private final ResourceLocation primitivemap = new ResourceLocation("footsteps", "primitivemap.cfg");
+	private final ResourceLocation variator = new ResourceLocation("footsteps", "variator.cfg");
+
+	// Resource pack reference for the built in pack.
+	private static class DefaultPack implements IResourcePack {
+
+		@Override
+		public InputStream getInputStream(final ResourceLocation loc) throws IOException {
+			final StringBuilder builder = new StringBuilder();
+			builder.append("/assets/dsurround/data/");
+			builder.append(loc.getResourceDomain());
+			builder.append('/');
+			builder.append(loc.getResourcePath());
+			return Module.class.getResourceAsStream(builder.toString());
+		}
+
+		@Override
+		public boolean resourceExists(final ResourceLocation loc) {
+			return true;
+		}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Override
+		public Set getResourceDomains() {
+			return null;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public IMetadataSection getPackMetadata(final IMetadataSerializer loc, final String data) throws IOException {
+			return null;
+		}
+
+		@Override
+		public BufferedImage getPackImage() throws IOException {
+			return null;
+		}
+
+		@Override
+		public String getPackName() {
+			return "DEFAULT";
+		}
+
+	}
+
+	public List<IResourcePack> findResourcePacks() {
 		final List<ResourcePackRepository.Entry> repo = Minecraft.getMinecraft().getResourcePackRepository()
 				.getRepositoryEntries();
 
-		final List<ResourcePackRepository.Entry> foundEntries = new ArrayList<ResourcePackRepository.Entry>();
+		final List<IResourcePack> foundEntries = new ArrayList<IResourcePack>();
+		foundEntries.add(new DefaultPack());
 
 		for (final ResourcePackRepository.Entry pack : repo) {
+			ModLog.debug("Resource Pack: %s", pack.getResourcePackName());
 			if (checkCompatible(pack)) {
-				foundEntries.add(pack);
-			}
-		}
-		return foundEntries;
-	}
-
-	public List<ResourcePackRepository.Entry> findDisabledResourcePacks() {
-		final ResourcePackRepository rrr = Minecraft.getMinecraft().getResourcePackRepository();
-
-		final List<ResourcePackRepository.Entry> repo = new ArrayList<ResourcePackRepository.Entry>(
-				rrr.getRepositoryEntriesAll());
-		repo.removeAll(rrr.getRepositoryEntries());
-
-		final List<ResourcePackRepository.Entry> foundEntries = new ArrayList<ResourcePackRepository.Entry>();
-		for (final ResourcePackRepository.Entry pack : repo) {
-			if (checkCompatible(pack)) {
-				foundEntries.add(pack);
+				ModLog.debug("Found Footsteps resource pack: %s", pack.getResourcePackName());
+				foundEntries.add(pack.getResourcePack());
 			}
 		}
 		return foundEntries;
@@ -83,32 +117,22 @@ public class PFResourcePackDealer {
 	}
 
 	public InputStream openPackDescriptor(final IResourcePack pack) throws IOException {
-		if (pack == null)
-			return Module.class.getResourceAsStream("/assets/dsurround/data/footsteps/pf_pack.json");
 		return pack.getInputStream(this.pf_pack);
 	}
 
 	public InputStream openAcoustics(final IResourcePack pack) throws IOException {
-		if (pack == null)
-			return Module.class.getResourceAsStream("/assets/dsurround/data/footsteps/acoustics.json");
 		return pack.getInputStream(this.acoustics);
 	}
 
 	public InputStream openBlockMap(final IResourcePack pack) throws IOException {
-		if (pack == null)
-			return Module.class.getResourceAsStream("/assets/dsurround/data/footsteps/blockmap.cfg");
 		return pack.getInputStream(this.blockmap);
 	}
 
 	public InputStream openPrimitiveMap(final IResourcePack pack) throws IOException {
-		if (pack == null)
-			return Module.class.getResourceAsStream("/assets/dsurround/data/footsteps/primitivemap.cfg");
 		return pack.getInputStream(this.primitivemap);
 	}
 
 	public InputStream openVariator(final IResourcePack pack) throws IOException {
-		if (pack == null)
-			return Module.class.getResourceAsStream("/assets/dsurround/data/footsteps/variator.cfg");
 		return pack.getInputStream(this.variator);
 	}
 }
