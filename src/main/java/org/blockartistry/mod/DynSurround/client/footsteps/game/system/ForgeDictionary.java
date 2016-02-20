56f7cf70c0b5d417bfcfc5bb92c627e8438a705f
Joseph Caleb Jones <eyespot7b@aol.com>
2016-02-20 01:38:16 -0500

    Dictionary tweaks
    Added more support for various mods via the oredictionary until more progress begins on it

--- src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/game/system/ForgeDictionary.java
+++ src/main/java/org/blockartistry/mod/DynSurround/client/footsteps/game/system/ForgeDictionary.java
@@ -43,14 +43,15 @@ public final class ForgeDictionary {
 
 	private static final String[] oreBlocks = { "oreIron", "oreGold", "oreCopper", "oreTin", "oreSilver", "oreLead",
 			"oreNickle", "orePlatinum", "oreManaInfused", "oreElectrum", "oreInvar", "oreBronze", "oreSignalum",
-			"oreEnderium", "oreLapis", "oreDiamond", "oreEmerald", "oreRedstone", "oreQuartz", "oreCoal" };
+			"oreEnderium", "oreLapis", "oreDiamond", "oreEmerald", "oreRedstone", "oreQuartz", "oreCoal", "oreNickel",
+			"oreChimerite", "oreBlueTopaz", "oreMoonstone", "oreVinteum", "oreSunstone" };
 
 	private static final String[] metalBlocks = { "blockIron", "blockGold", "blockCopper", "blockTin", "blockSilver",
 			"blockLead", "blockNickle", "blockPlatinum", "blockMithril", "blockElectrum", "blockInvar", "blockBronze",
-			"blockSignalum", "blockLumium", "blockEnderium", "blockSteel" };
+			"blockSignalum", "blockLumium", "blockEnderium", "blockSteel", "blockNickel" };
 
 	private static final String[] woodBlocks = { "logWood", "planksWood", "slabWood", "stairWood", "plankBamboo",
-			"slabBamboo", "stairBamboo" };
+			"slabBamboo", "stairBamboo", "craftingTableWood" };
 
 	private static final String[] saplings = { "treeSaplings", "saplingTree" };
 
@@ -60,15 +61,15 @@ public final class ForgeDictionary {
 
 	private static final String[] stoneBlocks = { "stone", "cobblestone" };
 
-	private static final String[] sandstoneBlocks = { "sandstone" };
+	private static final String[] sandstoneBlocks = { "sandstone", "blockPrismarine", "blockVinteum", "blockChimerite", "blockBlueTopaz", "blockMoonstone", "blockSunstone" };
 
 	private static final String[] sandBlocks = { "sand" };
 
 	private static final String[] woodChests = { "chestWood", "chestTrapped" };
 
-	private static final String[] rugBlocks = { "wool", "blockClothRock" };
+	private static final String[] rugBlocks = { "wool", "blockClothRock", "materialBedding" };
 
-	private static final String[] fenceBlocks = { "fenceWood" };
+	private static final String[] fenceBlocks = { "fenceWood", "fenceGateWood" };
 
 	private static final Map<String, String[]> dictionaryMaps = new HashMap<String, String[]>();
 
