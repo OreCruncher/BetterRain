d0f4476c1ab5d1855bdc614572ccdcea80f6ea1c
OreCruncher <tedjkaiser@gmail.com>
2016-03-11 08:00:22 -0800

    Block bison sound by default

--- src/main/java/org/blockartistry/mod/DynSurround/ModOptions.java
+++ src/main/java/org/blockartistry/mod/DynSurround/ModOptions.java
@@ -290,10 +290,10 @@ public final class ModOptions {
 	@RestartRequired
 	public static String[] culledSounds = { "^minecraft:liquid.*", "minecraft:mob.sheep.say",
 			"minecraft:mob.chicken.say", "minecraft:mob.cow.say", "minecraft:mob.pig.say" };
-	@Parameter(category = CATEGORY_SOUND, property = CONFIG_BLOCKED_SOUNDS, defaultValue = "")
+	@Parameter(category = CATEGORY_SOUND, property = CONFIG_BLOCKED_SOUNDS, defaultValue = "dsurround:bison")
 	@Comment("Sounds to block from playing")
 	@Hidden
-	public static String[] blockedSounds = {};
+	public static String[] blockedSounds = { "dsurround:bison" };
 	@Parameter(category = CATEGORY_SOUND, property = CONFIG_SOUND_VOLUMES, defaultValue = "")
 	@Comment("Individual sound scaling factors")
 	@Hidden
